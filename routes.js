f89fcd32dd7408934475b483c7bfc0bc6fffd2b4
Michael Harvey <wharvey@mymail.mines.edu>
2015-02-07 18:54:35 -0700

    messing around with routes

---
+++ routes.js
@@ -0,0 +1,15 @@
+// routes
+
+Router.route('/', function () {
+  this.render('Home');
+});
+
+Router.route('/profile/:_id', function () {
+  this.render('Profile', {
+    data: function () {
+      templateData = Meteor.users.findOne({_id: this.params._id});
+      console.log(templateData);
+      return templateData;
+    }
+  });
+});
