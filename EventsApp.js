0968079594a2ae7ef7e0ebc4240f7539607e4881
Michael Harvey <wharvey@mymail.mines.edu>
2015-02-17 23:21:06 -0700

    Added more boilerplate including layout, loading, and notFound templates

--- EventsApp.js
+++ EventsApp.js
@@ -1,10 +1,10 @@
 posts = new Meteor.Collection('posts');
 
 if (Meteor.isClient) {
-  // At the bottom of the client code
-  Accounts.ui.config({
-    passwordSignupFields: "USERNAME_AND_EMAIL"
-  });
+	// At the bottom of the client code
+	Accounts.ui.config({
+		passwordSignupFields: "USERNAME_AND_EMAIL"
+	});
 	
 	Template.Posts.helpers({
 		'posts':function(){
@@ -21,26 +21,26 @@ if (Meteor.isClient) {
 	});
 	
 	Template.newPost.events({
-    'submit form.newPostForm': function(event){
-      event.preventDefault();
-      var title = event.target.newtitle.value;
-      var imgURL = event.target.newimgURL.value;
-      var descp = event.target.newdescription.value;
-      var time = event.target.newtime.value;
-      var location = event.target.newlocation.value;
-      Meteor.call('insertNewEvent', title, descp, imgURL, time, location);
-    }
-  });
+		'submit form.newPostForm': function(event){
+			event.preventDefault();
+			var title = event.target.newtitle.value;
+			var imgURL = event.target.newimgURL.value;
+			var descp = event.target.newdescription.value;
+			var time = event.target.newtime.value;
+			var location = event.target.newlocation.value;
+			Meteor.call('insertNewEvent', title, descp, imgURL, time, location);
+		}
+	});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Meteor.startup(function () {
+		// code to run on server at startup
+	});
 	
 	Meteor.methods({
-    'insertNewEvent': function(title, descp, imgURL, time, location){
-		  posts.insert({
+		'insertNewEvent': function(title, descp, imgURL, time, location){
+			posts.insert({
 			title: title,
 			author: Meteor.user().username,
 			description: descp,
@@ -49,7 +49,7 @@ if (Meteor.isServer) {
 			imgURL: imgURL,
 			rsvp: 0,
 			userlist: []
-		  });
+			});
 		}
 	});
 }
