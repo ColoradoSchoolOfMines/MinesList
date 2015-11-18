3454312e0a70c2facae0d3dd89b74371878400fb
Kento Okamoto <oka.keno@gmail.com>
2015-11-17 19:53:02 -0700

    Name Events changed to Posts

---
+++ client/templates/posts/post_item.js
@@ -0,0 +1,8 @@
+Template.postItem.helpers({
+
+});
+
+Template.postItem.events({
+
+});
+
---
+++ client/templates/posts/post_new.js
@@ -0,0 +1,31 @@
+Session.set('uploadUrl', null);
+
+Template.postNew.helpers({
+	finishedUpload: function() {
+    return {
+        finished: function(index, fileInfo, context) { 
+        	console.log(fileInfo.url);
+        	Session.set('uploadUrl', fileInfo.url);
+         }
+    }
+  },
+  getUploadUrl: function(){
+  	return Session.get('uploadUrl');
+  }
+});
+
+Template.postNew.events({
+	'submit form': function(post) {
+		post.preventDefault();
+		var p = {
+			title: post.target.title.value,
+			imgURL: post.target.imgURL.value,
+			desc: post.target.description.value,
+			location: post.target.location.value
+		}
+		
+		console.log("title");
+		console.log("check");
+		Meteor.call('insertPost', p);
+	}
+});
