0be423a54d00152c6c86eee80a3d97a7455b58a8
Michael Harvey <wharvey@mymail.mines.edu>
2016-01-26 19:51:19 -0700

    added ability to favorite posts; database only

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -7,5 +7,8 @@ Template.postItem.helpers({
 Template.postItem.events({
 	'click .post': function(post) {
 		Router.go('post', { postId: this._id });
-	}
+	},
+    'click .favoriteStar': function() {
+        Meteor.call("favoritePost", this);
+    }
 });
--- server/methods.js
+++ server/methods.js
@@ -10,6 +10,18 @@ Meteor.methods({
 			created_at: new Date(),
 			cancel: { is_cancelled: false, reason: '' }
 		});
-		console.log("Serverside: Check");
+	},
+	'favoritePost': function(post) {
+		alreadyExists = false;
+
+		Meteor.user().favorites.forEach(function(favPost) {
+			if (favPost._id == post._id) {
+				alreadyExists = true;
+			}
+		});
+
+		if (!alreadyExists) {
+			Meteor.users.update({ _id: Meteor.user()._id },{ $push: { favorites: post }});
+		}
 	}
 });
